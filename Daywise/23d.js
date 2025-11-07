4fa99a7b601240c0ae4c978e2c2fd8a096bad192
jeevant010 <jeevantmudgil10@gmail.com>
2025-11-07 09:20:45 +0530

    destructuring and some concepts of arrow functions

--- Daywise/23d.js
+++ Daywise/23d.js
@@ -21,4 +21,50 @@ const add1 = function (a, b) {
 
 const arr = [1, 2, 3, 4, 5];
 
-const a2 = arr.
+const a2 = arr.map(i => i);
+console.log(a2);
+
+
+const Car =  {
+    make : "Mercedes-Benz",
+    model : "W13",
+
+    move : function() {
+        console.log(this)
+    },
+    move2 : function() {
+        console.log(this)
+    }
+}
+
+/**
+ * 
+ * Array function cannot be a factory function as we can declare 
+ * 
+ * const Car = () => {
+ * make = "Name",
+ * model = "F34",
+ * move : function(){
+ *  console.log(this)
+ * }
+ * }
+ */
+
+// Car.move()
+// Car.move2()
+
+// console.log(Car)
+
+/**Destructing */
+
+// const a = ["a" , "b" , "c"];
+// const a1 = a[0];
+// const a21 = a[1];
+// const a3 = a[2];
+
+// const [ b1, b2, b3 ] = a;
+// const [ aa, bb , cc, dd] = a;
+// console.log(b1, b2, b3);
+
+// console.log(aa, bb, cc, dd);
+
