fee24ec2859a3685fcea62d6ee98844bf52f0592
jeevant010 <jeevantmudgil10@gmail.com>
2025-11-07 09:10:58 +0530

    some topics about array function

---
+++ Daywise/23d.js
@@ -0,0 +1,21 @@
+// function greet(Name = 'hello'){
+//     console.log('hi ' + Name)
+// }
+
+// greet()
+
+// greet('name')
+
+// const greet = (Name = 'hello') => {
+//     console.log('hi ' + Name);
+// }
+
+const add = (a, b) => a+b;
+console.log(add(5 , 6));
+
+const add1 = function (a, b) {
+    return a + b;
+}
+// console.log(add1([5 , 6], [4, 6]));
+
+
