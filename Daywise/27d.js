4424d7ebf32d72aca3a7c1a13ca09341ed68458a
jeevant010 <jeevantmudgil10@gmail.com>
2025-11-08 22:11:57 +0530

    More setup to do practise coding

--- Daywise/27d.js
+++ Daywise/27d.js
@@ -1,6 +1,12 @@
-// DOM
+// DOM Now we are manupulating DOM
 console.log("i am wiriting")
 
 for(let i=0; i<100; i++){
     console.log("Written");
 }
+
+console.log(document)
+const ele = document.getElementById("root");
+console.log(ele);
+
+ele.innerHTML("hello");
