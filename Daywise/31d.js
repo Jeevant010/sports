d19eec2df1ce0e29a94a80f680050dc46d8c2b7c
jeevant010 <jeevantmudgil10@gmail.com>
2025-11-11 00:24:51 +0530

    work to create a recipe application to be targetted to complete tomorrow so lets go

---
+++ Daywise/31d.js
@@ -0,0 +1,21 @@
+const api_id = "225008eb"
+
+const api_key = "180043898e6fe8ad6b165f9dec4b3530"
+
+// const string = '2*8*10-2';
+// const result = eval(string);
+// console.log(result);
+
+const query = document.querySelector('header.search input');
+const search = document.querySelector("header.search button");
+
+const handleSearch = async () => {
+    try{
+        console.log("search");
+    }       
+    catch{
+
+    }
+}
+
+search.addEventListener("click", handleSearch);
