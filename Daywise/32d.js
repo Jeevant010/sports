80993e46477d0e455836b5bef8b65a8f98d50d92
jeevant010 <jeevantmudgil10@gmail.com>
2025-11-29 12:09:03 +0530

    finished day 32 using dark mode

---
+++ Daywise/32d.js
@@ -0,0 +1,16 @@
+/***
+ * 
+ * DARK MODE
+ */
+
+const body = document.querySelector('body');
+
+const changeMode = () => {
+    // if( !body.classList.contains('dark')){
+    //     body.classList.add('dark');
+    // }
+    // else {
+    //     body.classList.remove('dark');
+    // }
+    body.classList.toggle('dark');
+}
